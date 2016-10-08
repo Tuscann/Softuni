bbf361aef8b4db1fe41c1d0c3418f3a23b2f071b
Tuscann <fbinnzhivko@gmail.com>
2016-10-08 14:05:29 +0300

    a
    a

---
+++ JS core/Exams/JavaScript Basics Retake - 11 May 2016/01. Medenka Wars.js
@@ -0,0 +1,6 @@
+function medenkaWars(input) {
+
+    console.log(input);
+}
+medenkaWars(['2 dark medenkas', '1 white medenkas', '2 dark medenkas', '2 dark medenkas', '15 white medenkas', '2 dark medenkas', '2 dark medenkas']);
+medenkaWars(['5 white medenkas', '5 dark medenkas', '4 white medenkas']);
