ad0479e0929de77715e4b290bb999a94bddb2d17
Tuscann <fbinnzhivko@gmail.com>
2016-10-28 20:34:44 +0300

    a
    a

--- JS core/JavaScript Advanced/10.Lab Classes and Members/04. Circle.js
+++ JS core/JavaScript Advanced/10.Lab Classes and Members/04. Circle.js
@@ -1,3 +1,24 @@
-/**
- * Created by 123 on 28/10/2016.
- */
+class Circle {
+    constructor(raduis) {
+        this.radius = raduis;
+    }
+    get diameter() {
+        return 2 * this.radius;
+    }
+    set diameter(diameter) {
+        this.radius = diameter / 2;
+    }
+    get area() {
+        return Math.PI * this.radius * this.radius;
+    }
+}
+let c = new Circle(2);
+console.log(`Radius: ${c.radius}`);
+console.log(`Diameter: ${c.diameter}`);
+console.log(`Area: ${c.area}`);
+c.diameter = 1.6;
+console.log(`Radius: ${c.radius}`);
+console.log(`Diameter: ${c.diameter}`);
+console.log(`Area: ${c.area}`);
+
+
--- JS core/JavaScript Advanced/10.Lab Classes and Members/05. Point Distance.js
+++ JS core/JavaScript Advanced/10.Lab Classes and Members/05. Point Distance.js
@@ -1,3 +1,15 @@
-/**
- * Created by 123 on 28/10/2016.
- */
+class Point {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    static distance(a, b) {
+        const dx = a.x - b.x;
+        const dy = a.y - b.y;
+        return Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2))
+    }
+}
+let p1 = new Point(5, 5);
+let p2 = new Point(10, 10);
+console.log(Point.distance(p1, p2));
